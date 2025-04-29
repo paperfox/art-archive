9b774b448815717a489f85ff1203934576802bbc
Nathalie Garfinkle <ndg.paperfox@gmail.com>
2025-04-28 22:05:46 -0400

    update url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://paperfox.github.io/art/",
+  base: "https://paperfox.github.io/art-archive/",
   server: {
     mimeTypes: {
       "application/json": ["json"],
